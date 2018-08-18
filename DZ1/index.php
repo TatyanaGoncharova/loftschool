342f9924b6a41a78a64cc5f0f0791cc19f19a03a
Tanya <7371056@gmail.com>
2018-08-18 19:42:41 +0300

    dz1

---
+++ DZ1/index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Gusya
+ * Date: 18.08.2018
+ * Time: 16:27
+ */
+include "src.php";
+
+$user_name = "Igor";
+
+if (1==1) {
+    echo 'hi';
+}
---
+++ DZ1/src.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Gusya
+ * Date: 18.08.2018
+ * Time: 18:46
+ */
+const TEST_CONSTANTA = 'asd';
+
+function show_something()
+{
+    return 'SOMETHING';
+}
